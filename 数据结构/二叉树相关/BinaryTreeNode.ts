0773dd6ce70cddb1ce08b57881dbaa47ed31df09
GitHub AaronY666 <GitHub yedongdong2019@163.com>
2020-11-12 17:52:51 +0800

    add create BinaryTreeNode

---
+++ 数据结构/二叉树相关/BinaryTreeNode.ts
@@ -0,0 +1,14 @@
+// class BinaryTreeNode {
+//     public value: any = null;
+//     public left: BinaryTreeNode = null;
+//     public right: BinaryTreeNode = null;
+//     constructor(value) {
+//         this.value = value;
+//     }
+// }
+
+function BinaryTreeNode(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
