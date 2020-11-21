bc1359cb7f23c580178a43d61bfee38648e2195a
AaronY666 <yedongdong2019@163.com>
2020-11-21 23:16:21 +0800

    addd 后序遍历

--- 数据结构/二叉树相关/后序遍历.ts
+++ 数据结构/二叉树相关/后序遍历.ts
@@ -13,18 +13,37 @@ function postTraversal2(node) {
         return;
     }
 
-    let stack = []
+    let stack = [], last = null;
     while (node || stack.length) {
         while (node) {
             stack.push(node);
             node = node.left;
         }
-        //一直到左边节点没有，回到根节点,这时候根节点肯定有值
-        node = stack.pop();
+        //一直到左边节点没有，回到根节点,这时候根节点肯定有值，不急着出栈
+        node = stack[stack.length - 1];
 
-        //这时候要继续找右边的节点
-        node = node.right;
+        //如果右子树父节点被遍历过了，或者为空，那么就说明左右都遍历完了，可以输出中节点并弹栈
+        if (node.right === last || !node.right) {
+            //开始弹栈
+            node = stack.pop();
+            //输出中节点
+            console.log(node.value);
+
+            //为了避免重复向右查找，更新last
+            last = node;
+
+            //为了避免重复向左查找，把节点设为null,这样就不会向左查找了
+            node = null;
+
+
+        } else {
+            //否则的话要继续找右边的节点
+            node = node.right;
+        }
+
     }
+
+
 
 
 }
