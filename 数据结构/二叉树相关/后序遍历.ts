e29237a095a9c27a63e403f654ec6f59d00909b3
AaronY666 <yedongdong2019@163.com>
2020-11-22 14:32:20 +0800

    add 后序遍历.md

--- 数据结构/二叉树相关/后序遍历.ts
+++ 数据结构/二叉树相关/后序遍历.ts
@@ -5,7 +5,7 @@ function postTraversal(node) {
     }
     postTraversal(node.left);
     postTraversal(node.right);
-    console.log(node);
+    console.log(node.value);
 }
 
 function postTraversal2(node) {
@@ -19,22 +19,21 @@ function postTraversal2(node) {
             stack.push(node);
             node = node.left;
         }
-        //一直到左边节点没有，回到根节点,这时候根节点肯定有值，不急着出栈
+        //一直到左边节点没有，回到根节点,这时候根节点肯定有值，不急着出栈，因为这个点还没被输出过
         node = stack[stack.length - 1];
 
-        //如果右子树父节点被遍历过了，或者为空，那么就说明左右都遍历完了，可以输出中节点并弹栈
+        //如果右子树父节点被遍历过了||或者为空，那么就说明左右都遍历完了，可以输出中节点并弹栈
         if (node.right === last || !node.right) {
             //开始弹栈
             node = stack.pop();
             //输出中节点
             console.log(node.value);
 
-            //为了避免重复向右查找，更新last
+            //last设置为当前节点，是为了从右边节点返回到父节点时，避免再次向右查找，所以同样的判断条件里也得加上node.right===last
             last = node;
 
-            //为了避免重复向左查找，把节点设为null,这样就不会向左查找了
+            //node设置为null是为了相当于把这个点及其子节点数都屏蔽掉，当做空节点处理，这样就会返回父节点，并且去查找父节点的右节点
             node = null;
-
 
         } else {
             //否则的话要继续找右边的节点
@@ -42,8 +41,4 @@ function postTraversal2(node) {
         }
 
     }
-
-
-
-
 }
